48a187f35da29d649005354e7bdecf9a2827e27e
ubaid4j <ubaidshan007@gmail.com>
2023-04-13 15:01:26 +0500

    - add spring cloud stream binder tests

---
+++ src/test/java/dev/ubaid/dispatcherservice/FunctionStreamIntegrationTests.java
@@ -0,0 +1,51 @@
+package dev.ubaid.dispatcherservice;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dev.ubaid.dispatcherservice.dto.OrderAcceptedMessage;
+import dev.ubaid.dispatcherservice.dto.OrderDispatchedMessage;
+import java.io.IOException;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.stream.binder.test.InputDestination;
+import org.springframework.cloud.stream.binder.test.OutputDestination;
+import org.springframework.context.annotation.Import;
+import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+
+
+
+@SpringBootTest
+@Import(TestChannelBinderConfiguration.class)
+public class FunctionStreamIntegrationTests {
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiring-Inspection")
+    @Autowired
+    private InputDestination input;
+
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiring-Inspection")
+    @Autowired
+    private OutputDestination output;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void whenOrderAcceptedThenDispatched() throws IOException {
+        long orderId = 122;
+
+        Message<OrderAcceptedMessage> inputMessage = MessageBuilder
+            .withPayload(new OrderAcceptedMessage(orderId)).build();
+
+        Message<OrderDispatchedMessage> expectedOutputMessage = MessageBuilder
+            .withPayload(new OrderDispatchedMessage(orderId)).build();
+
+        this.input.send(inputMessage);
+
+        Assertions.assertThat(objectMapper.readValue(output.receive().getPayload(), OrderDispatchedMessage.class))
+            .isEqualTo(expectedOutputMessage.getPayload());
+    }
+
+}
